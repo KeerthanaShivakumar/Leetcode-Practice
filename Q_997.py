3dd3ade166d5009106e66c15a4c178ad1f3b7336
Keerthana Shivakumar <shivakumarkeerthana@gmail.com>
2023-02-16 08:53:01 +0530

    Find the town judge

---
+++ Q_997.py
@@ -0,0 +1,11 @@
+#997. find the town judge
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        count = [0]*(n+1)
+        for a, b in trust:
+            count[a]-=1 #doesnt trust oneself
+            count[b]+=1 #trusts this
+        for i in range(1, n+1):
+            if(count[i]==n-1):
+                return i
+        return -1        
